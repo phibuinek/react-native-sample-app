dcc8b5fc412cd8fc3639d617fc7631e74921c770
phibuinek <phibuinek@gmail.com>
2025-05-13 19:45:20 +0700

    view, text, styles react native dont have css

--- App.tsx
+++ App.tsx
@@ -4,17 +4,36 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World with Phi Bùi It!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.header}>Phi Bùi It</Text>
+      </View>
+      <Text style={styles.hello1}>Hello World with Phi Bùi It! 1</Text>
+      <Text>Hello World with Phi Bùi It! 2</Text>
     </View>
   );
 }
 
+//ko có khái niệm css // css in js
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  hello1: {
+    color: "red",
+    fontSize: 60,
+    borderColor: "green",
+    borderWidth: 1,
+    padding: 10,
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  parent: {
+    fontSize: 60,
+    color: "green",
   },
 });
